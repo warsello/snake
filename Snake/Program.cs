d6eecac425c731541f703a83bff6fc0ccb691c51
warsello <gm131@mail.ru>
2018-09-11 22:18:51 +0300

    p1.Draw();

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,20 +10,26 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1, y1 = 3;
+            Point p1 = new Point();
+            p1.x = 1; p1.y = 3; p1.sym = '*';
+            p1.Draw();
+           /* int x1 = 1, y1 = 3;
             char sym1 = '*';
-            Draw(x1,y1,sym1);
+            Draw(x1,y1,sym1);*/
+            Point p2 = new Point();
+            p2.x = 3; p2.y = 5; p2.sym = '#';
+            p2.Draw();
 
-            int x2 = 3, y2= 5;
+/*            int x2 = 3, y2= 5;
             char sym2 = '#';
-            Draw(x2,y2,sym2);
+            Draw(x2,y2,sym2);*/
             Console.ReadLine();
         }
-            static void Draw(int x, int y, char sym)
+           /* static void Draw(int x, int y, char sym)
             {
                Console.SetCursorPosition(x, y);
                Console.Write(sym);
 
-            }
+            }*/
     }
 }
